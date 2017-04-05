403cbad4926ea30e594579e047934913d4906b44
Anastasia <ngoral@304.ru>
2017-04-06 00:52:29 +0300

    End w1t1 code

--- Week 1/Task 1/change_levels.cpp
+++ Week 1/Task 1/change_levels.cpp
@@ -6,9 +6,11 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
+#define STD_LEVELS_NUMBER 256
+
 std::string getImageName(int argc, char** argv)
 {
-    return (argc > 2) ? argv[1] : "no_signal.png";
+    return (argc > 2) ? argv[2] : "no_signal.png";
 }
 
 bool isRightLevelValue(char* value)
@@ -17,7 +19,7 @@ bool isRightLevelValue(char* value)
     return v >= 2 && v <= 256 && ((v) & (v-1)) == 0;
 }
 
-int levelsNumber(int argc, char** argv)
+unsigned short int levelsNumber(int argc, char** argv)
 {
     if (argc > 1)
     {
@@ -38,13 +40,41 @@ cv::Mat readImage(const std::string& imageName)
     return image;
 }
 
+void showImage(const cv::Mat& image)
+{
+    cv::namedWindow("Change levels", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Change levels", image);
+    cv::waitKey(0);
+}
+
+std::string filenameExtension(const std::string& filename)
+{
+    size_t dot = filename.rfind('.');
+    size_t basename = filename.rfind('/') + 1;
+    return (dot != basename) ? filename.substr(dot) : "";
+}
+
+cv::Mat changeLevels(const cv::Mat& original, unsigned short int levels)
+{
+    cv::Mat newImage = original.clone();
+    unsigned short int koefficient = STD_LEVELS_NUMBER / levels; //always power of 2
+
+    for(auto pixel = newImage.begin<uchar>(), end = newImage.end<uchar>(); pixel != end; ++pixel)
+        *pixel = (*pixel / koefficient) * koefficient;
+
+    return newImage;
+}
+
 int main(int argc, char** argv)
 {
     try {
-        int levels = levelsNumber(argc, argv);
+        unsigned short int levels = levelsNumber(argc, argv);
         std::string imageName = getImageName(argc, argv);
         cv::Mat image = readImage(imageName);
+        cv::Mat newImage = changeLevels(image, levels);
         std::cout << levels << ";" << imageName << std::endl;
+        cv::imwrite("changed" + filenameExtension(imageName), newImage);
+        showImage(newImage);
         return 0;
     }
     catch (const std::exception& e) {
