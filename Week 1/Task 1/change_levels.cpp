a1fa2c1da2ad23290b0da4e3ddba5dc2c7c0334e
Anastasia <ngoral@304.ru>
2017-04-06 01:17:34 +0300

    Add .h for reusable functions

--- Week 1/Task 1/change_levels.cpp
+++ Week 1/Task 1/change_levels.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include "../../improc.h"
 
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
 #define STD_LEVELS_NUMBER 256
@@ -30,28 +30,6 @@ unsigned short int levelsNumber(int argc, char** argv)
     }
     else
         throw std::runtime_error("Could not find intensity levels number. Usage: ./change_levels levels_number [image_name] ");
-}
-
-cv::Mat readImage(const std::string& imageName)
-{
-    cv::Mat image = cv::imread(imageName, cv::IMREAD_GRAYSCALE); //Read the file (grayscale image)
-    if (image.empty())                                           //Check for invalid input
-        throw std::runtime_error("Could not open or find the image `" + imageName + "'");
-    return image;
-}
-
-void showImage(const cv::Mat& image)
-{
-    cv::namedWindow("Change levels", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Change levels", image);
-    cv::waitKey(0);
-}
-
-std::string filenameExtension(const std::string& filename)
-{
-    size_t dot = filename.rfind('.');
-    size_t basename = filename.rfind('/') + 1;
-    return (dot != basename) ? filename.substr(dot) : "";
 }
 
 cv::Mat changeLevels(const cv::Mat& original, unsigned short int levels)
