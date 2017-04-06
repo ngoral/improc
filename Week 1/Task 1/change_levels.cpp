d32245ba408c300095e2a78ddb8e48215701319b
Anastasia <ngoral@304.ru>
2017-04-06 03:04:49 +0300

    Refactor input check

--- Week 1/Task 1/change_levels.cpp
+++ Week 1/Task 1/change_levels.cpp
@@ -6,23 +6,28 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+static const unsigned STD_LEVELS_NUMBER = 256;
 
 std::string getImageName(int argc, char** argv)
 {
     return (argc > 2) ? argv[2] : "no_signal.png";
 }
 
-bool isRightLevelValue(char* value)
+bool isPowerOfTwo(unsigned number)
 {
-    int v = std::stoi(value);
-    return v >= 2 && v <= 256 && ((v) & (v-1)) == 0;
+    return (number & (number - 1)) == 0;
+}
+
+bool LevelValid(unsigned value)
+{
+    return value >= 2 && value <= 256 && isPowerOfTwo(value);
 }
 
 unsigned short levelsNumber(int argc, char** argv)
 {
     if (argc > 1)
     {
-        if (isRightLevelValue(argv[1]))
+        if (LevelValid(std::stoi(argv[1])))
             return std::stoi(argv[1]);
         else
             throw std::runtime_error("Wrong intensity levels number `" + std::string(argv[1]) + "'");
