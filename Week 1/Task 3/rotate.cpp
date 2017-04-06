df34fabce5d329fffd23f9b2601cc9f27459db81
Anastasia <ngoral@304.ru>
2017-04-06 03:15:21 +0300

    Init W1T

---
+++ Week 1/Task 3/rotate.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "improc.h"
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
