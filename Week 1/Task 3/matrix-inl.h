6508263737273fd69b4ead40848c44d63530346f
Anastasia <ngoral@304.ru>
2017-05-09 21:10:33 +0300

    Make methods inline

--- Week 1/Task 3/matrix-inl.h
+++ Week 1/Task 3/matrix-inl.h
@@ -8,28 +8,28 @@
 
 
 template <class T>
-Matrix<T>::Matrix():
+inline Matrix<T>::Matrix():
     height_(0),
     width_(0),
     matrix_(0,0)
 {}
 
 template <class T>
-Matrix<T>::Matrix(int height, int width, std::vector<T> matrix):
+inline Matrix<T>::Matrix(int height, int width, std::vector<T> matrix):
     height_(height),
     width_(width),
     matrix_(matrix)
 {}
 
 template <class T>
-Matrix<T>::Matrix(int height, int width, T matrix[]):
+inline Matrix<T>::Matrix(int height, int width, T matrix[]):
     height_(height),
     width_(width),
     matrix_(matrix.begin(), matrix.end())
 {}
 
 template <class T>
-Matrix<T>::Matrix(const Matrix<T>& matrix):
+inline Matrix<T>::Matrix(const Matrix<T>& matrix):
     height_(matrix.height()),
     width_(matrix.width()),
     matrix_(matrix.elements())
@@ -40,7 +40,7 @@ Matrix<T>::Matrix(const Matrix<T>& matrix):
 // ===========================
 
 template <class T>
-std::ostream& operator<<(std::ostream &os, const Matrix<T> &matrix)
+inline std::ostream& operator<<(std::ostream &os, const Matrix<T> &matrix)
 {
     os << "[\n ";
     for (int i = 0; i != matrix.height(); ++i) {
@@ -61,7 +61,7 @@ std::ostream& operator<<(std::ostream &os, const Matrix<T> &matrix)
 }
 
 template <class T>
-Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2)
+inline Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2)
 {
     if (matrix1.width() != matrix2.width() || matrix1.height() != matrix2.height()) {
         throw std::runtime_error("Matrices dimensions mismatch");
@@ -78,7 +78,7 @@ Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2)
 }
 
 template <class T>
-Matrix<T> operator*(const Matrix<T> &matrix1, const Matrix<T> &matrix2)
+inline Matrix<T> operator*(const Matrix<T> &matrix1, const Matrix<T> &matrix2)
 {
     if (matrix1.width() != matrix2.height()) {
         throw std::runtime_error("Matrices dimensions mismatch");
--- Week 1/Task 3/matrix.h
+++ Week 1/Task 3/matrix.h
@@ -5,18 +5,18 @@
 
 template <class T> class Matrix {
 public:
-    Matrix();
-    Matrix(int height, int width, std::vector<T> matrix);
-    Matrix(int height, int width, T matrix[]);
-    Matrix(const Matrix<T>& matrix);
+    inline Matrix();
+    inline Matrix(int height, int width, std::vector<T> matrix);
+    inline Matrix(int height, int width, T matrix[]);
+    inline Matrix(const Matrix<T>& matrix);
 
     //getters
-    const std::vector<T>& elements() const { return matrix_; }
-    int height() const { return height_; }
-    int width() const { return width_; }
+    inline const std::vector<T>& elements() const { return matrix_; }
+    inline int height() const { return height_; }
+    inline int width() const { return width_; }
 
     //setters
-    void setElements (const std::vector<T>& elements) { matrix_ = elements; }
+    inline void setElements (const std::vector<T>& elements) { matrix_ = elements; }
 
 private:
     int width_, height_;
@@ -24,8 +24,10 @@ private:
 };
 
 template <class T>
-std::ostream& operator<<(std::ostream &os, const Matrix<T> &matrix);
+inline std::ostream& operator<<(std::ostream &os, const Matrix<T> &matrix);
 template <class T>
-Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2);
+inline Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2);
 template <class T>
-Matrix<T> operator*(const Matrix<T> &matrix1, const Matrix<T> &matrix2);
+inline Matrix<T> operator*(const Matrix<T> &matrix1, const Matrix<T> &matrix2);
+
+#include "matrix-inl.h"
